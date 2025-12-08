0cd2a1c1360fb78234777852ef5af45bf7bda20d
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-12-09 00:39:22 +0530

    remove warning in header component.

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ const navLinks = [
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container mx-auto flex h-14 items-center justify-between px-4 md:px-6">
         {/* LOGO / BRAND NAME */}
         <div className="flex items-center gap-2">
